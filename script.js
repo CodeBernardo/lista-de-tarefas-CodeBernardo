806f52e358238886c28a2d60e91dd525dcb71296
Bernardo Stein <stein.bernardo@proton.me>
2023-07-12 13:53:10 -0300

    Tarefa 2 - criado logica para adicionar novos elementos a lista, criado logica para definir visualmente as prioridades

--- script.js
+++ script.js
@@ -10,14 +10,26 @@ const tasks = [
   {
     title: "Beber água",
     type: "Normal",
-  },
+  }
 ];
+
+
 
 function createCard(taskInfo) {
   // Criando elementos necessários
   const taskCardItem = document.createElement("li");
   const taskCardContent = document.createElement("div");
+
   const taskTitle = document.createElement("span");
+
+  if(taskInfo.type === "Urgente") {
+    taskTitle.classList.add('span-urgent')
+  } else if(taskInfo.type === "Prioritário") {
+    taskTitle.classList.add('span-priority')
+  } else {
+    taskTitle.classList.add('span-normal')
+  }
+
   const taskDescription = document.createElement("p");
 
   // Adicionando o titulo da tarefa como texto do paragrafo
@@ -26,17 +38,17 @@ function createCard(taskInfo) {
   // Adicionando span e paragrafo a div
   taskCardContent.appendChild(taskTitle);
   taskCardContent.appendChild(taskDescription);
-
+  
   // Criando botão para deletar tarefa
   const buttonDelete = document.createElement("button");
-
+  
   // Adicionando icone ao botão
   buttonDelete.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
-
+  
   // Adicionando a div e o botão de deletar ao list item
   taskCardItem.appendChild(taskCardContent);
   taskCardItem.appendChild(buttonDelete);
-
+  
   return taskCardItem;
 }
 
@@ -44,9 +56,7 @@ function renderElements(taskList) {
   const htmlList = document.querySelector(".tasks");
 
   // Ajustar a lógica
-
   let card
-
   for(let i=0; i<taskList.length; i++) {
     card = createCard(taskList[i]);
     htmlList.appendChild(card);
@@ -54,3 +64,30 @@ function renderElements(taskList) {
 }
 
 renderElements(tasks);
+
+function renderLastElement(taskList) {
+  const htmlList = document.querySelector(".tasks")
+  let card = createCard(taskList)
+  htmlList.appendChild(card)
+  console.log(htmlList)
+}
+
+const btnSubmit = document.getElementById('btnSubmit')
+btnSubmit.addEventListener('click', function(e) {
+  
+  let task = e.target.form.elements.input_title.value
+  let priority = e.target.form.elements.input_priority.value
+  
+  let newTask = {
+    title: task,
+    type: priority
+  }
+  
+  renderLastElement(newTask)
+
+  e.preventDefault() 
+  e.stopPropagation()
+})
+
+
+
